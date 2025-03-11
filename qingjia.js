31f5e72781a0714ea28489781f632045cf78dd04
Simprole <29875719+cui0v0@users.noreply.github.com>
2025-03-11 19:03:16 +0800

    Update qingjia.js

--- qingjia.js
+++ qingjia.js
@@ -1,4 +1,324 @@
-let obj = JSON.parse($response.body)
+const template = {
+    "message" : "SUCCESS",
+    "datas" : {
+      "currApprover" : null,
+      "detail" : {
+        "approverName" : "余欢",
+        "outStatus" : false,
+        "id" : "4d427d629e6040d1a6651bf9c6fc16b8",
+        "sex" : "男",
+        "disclaimers" : "本人承诺填写的信息真实有效，并对本次提交请假申请的信息真实性负责。",
+        "isAttendance" : null,
+        "isOtherReason" : false,
+        "leaveApplyExplain" : null,
+        "lastOut" : null,
+        "totalDays" : "6小时",
+        "mustOut" : 0,
+        "isResumptionSelf" : null,
+        "leaveType" : "病假",
+        "dstStatus" : "4",
+        "isIotReport" : 0,
+        "reportLocation" : null,
+        "actStatus" : "8",
+        "ccInfo" : null,
+        "endDate" : "2024-09-14 20:00",
+        "urgencyMobile" : "",
+        "status" : "2",
+        "reportComment" : null,
+        "pressStatus" : false,
+        "isEarlyEnd" : "0",
+        "userId" : "220531050",
+        "locationType" : 0,
+        "isOverdue" : false,
+        "reportLatitude" : null,
+        "approverWid" : "92181825",
+        "reportApplyExplain" : null,
+        "isUseAutoReport" : 0,
+        "logInfos" : [
+          {
+            "status" : "1",
+            "approverWid" : null,
+            "nodeName" : null,
+            "approverName" : null,
+            "leaveId" : "4d427d629e6040d1a6651bf9c6fc16b8",
+            "extendId" : null,
+            "approveOption" : null,
+            "nodeType" : null,
+            "createTime" : "2024-09-14 11:24:24",
+            "delayStatus" : null
+          },
+          {
+            "status" : "2",
+            "approverWid" : null,
+            "nodeName" : null,
+            "approverName" : null,
+            "leaveId" : "4d427d629e6040d1a6651bf9c6fc16b8",
+            "extendId" : null,
+            "approveOption" : null,
+            "nodeType" : null,
+            "createTime" : "2024-09-14 14:01:12",
+            "delayStatus" : null
+          },
+          {
+            "status" : "3",
+            "approverWid" : null,
+            "nodeName" : null,
+            "approverName" : null,
+            "leaveId" : "4d427d629e6040d1a6651bf9c6fc16b8",
+            "extendId" : null,
+            "approveOption" : null,
+            "nodeType" : null,
+            "createTime" : "2024-09-14 16:10:30",
+            "delayStatus" : true
+          }
+        ],
+        "actEndTime" : "2024-09-14 20:00",
+        "actEndTimeDesc" : "6小时",
+        "reportLongitude" : null,
+        "leaveCreateTime" : "2024-09-14 11:24",
+        "destination" : "",
+        "leaveReason" : "眼睑发炎，眼睛疼需要休息",
+        "reportAttach" : [
+  
+        ],
+        "startDate" : "2024-09-14 14:00",
+        "approverOpinion" : "",
+        "startTime" : "2024-09-14 14:00",
+        "userName" : "崔鲡沣",
+        "reportStatus" : null,
+        "reportLogs" : [
+  
+        ],
+        "applyLocation" : {
+          "locationType" : 0,
+          "latitude" : "30.46865818763575",
+          "longitude" : "114.09595114323203",
+          "address" : "湖北省武汉市蔡甸区全力二路"
+        },
+        "endTime" : "2024-09-14 20:00",
+        "gender" : "1",
+        "approverId" : "20152167",
+        "applyAttach" : [
+  
+        ],
+        "leaveName" : "病假",
+        "userWid" : null,
+        "createTime" : "2024-09-14 11:24",
+        "studentMobile" : "17762516986",
+        "leaveHour" : "360",
+        "dormitoryInfo" : "北校区-观湖苑5-609-4",
+        "approvers" : [
+          {
+            "status" : "1",
+            "handled" : true,
+            "approveNode" : [
+              {
+                "nodeIndex" : null,
+                "approveWid" : null,
+                "nextNodeIndex" : null,
+                "flowWid" : null,
+                "nodeType" : null,
+                "approverName" : null,
+                "approveType" : null,
+                "approveOption" : null,
+                "approverWid" : null,
+                "nextApproverIndex" : null,
+                "nodeWid" : null,
+                "nodeName" : null,
+                "createTime" : "2024-09-14 11:24:24",
+                "approvers" : [
+  
+                ],
+                "delayStatus" : null,
+                "approverId" : null,
+                "status" : "1"
+              }
+            ],
+            "approver" : {
+              "userWid" : "1037291170",
+              "userId" : "220531050",
+              "userName" : "崔鲡沣"
+            },
+            "level" : null,
+            "approveType" : null,
+            "approveOption" : null,
+            "createTime" : "2024-09-14 11:24",
+            "delayStatus" : null
+          },
+          {
+            "status" : "2",
+            "handled" : true,
+            "approveNode" : [
+              {
+                "nodeIndex" : null,
+                "approveWid" : null,
+                "nextNodeIndex" : null,
+                "flowWid" : null,
+                "nodeType" : null,
+                "approverName" : null,
+                "approveType" : null,
+                "approveOption" : null,
+                "approverWid" : null,
+                "nextApproverIndex" : null,
+                "nodeWid" : null,
+                "nodeName" : null,
+                "createTime" : "2024-09-14 14:01:12",
+                "approvers" : [
+  
+                ],
+                "delayStatus" : null,
+                "approverId" : null,
+                "status" : "2"
+              }
+            ],
+            "approver" : {
+              "userWid" : "1037291170",
+              "userId" : "220531050",
+              "userName" : "崔鲡沣"
+            },
+            "level" : null,
+            "approveType" : null,
+            "approveOption" : null,
+            "createTime" : "2024-09-14 14:01",
+            "delayStatus" : null
+          },
+          {
+            "status" : "3",
+            "handled" : true,
+            "approveNode" : [
+              {
+                "nodeIndex" : "0",
+                "approveWid" : null,
+                "nextNodeIndex" : null,
+                "flowWid" : null,
+                "nodeType" : "1",
+                "approverName" : "余欢",
+                "approveType" : null,
+                "approveOption" : "",
+                "approverWid" : "92181825",
+                "nextApproverIndex" : null,
+                "nodeWid" : "1003",
+                "nodeName" : "辅导员",
+                "createTime" : "2024-09-14 16:10:30",
+                "approvers" : [
+                  {
+                    "userWid" : "92181825",
+                    "userId" : "20152167",
+                    "userName" : "余欢"
+                  }
+                ],
+                "delayStatus" : true,
+                "approverId" : "20152167",
+                "status" : "3"
+              }
+            ],
+            "approver" : {
+              "userWid" : "92181825",
+              "userId" : "20152167",
+              "userName" : "余欢"
+            },
+            "level" : "1",
+            "approveType" : null,
+            "approveOption" : "",
+            "createTime" : "2024-09-14 16:10",
+            "delayStatus" : true
+          }
+        ],
+        "ljDays" : null
+      },
+      "holidayAutoDefineVos" : [
+  
+      ],
+      "condition" : {
+        "shutDown" : false,
+        "allowWithdraw" : 1,
+        "reportTutorialUrl" : null,
+        "reportTutorialTitle" : null
+      },
+      "isExtend" : "1",
+      "nextApproveInfo" : null,
+      "leaveType" : {
+        "id" : "2259",
+        "attachmentNumberLimit" : null,
+        "leaveApplyExplain" : null,
+        "chooseAttendance" : 0,
+        "reasonWordLimit" : null,
+        "enableAttach" : 0,
+        "code" : 2,
+        "needApproval" : 1,
+        "reportApplyFlow" : 0,
+        "reportApply" : 0,
+        "disableTime" : 0,
+        "reportApplyExplain" : null,
+        "mustOut" : 0,
+        "name" : "病假",
+        "isShowQrNoApproval" : 0
+      },
+      "studentInfo" : {
+        "userId" : "220531050",
+        "gender" : 1,
+        "userWid" : "1037291170",
+        "userName" : "崔鲡沣",
+        "major" : "机器人工程",
+        "grade" : "2022",
+        "cls" : "22机器人工程(本科)(创新)",
+        "dorm" : "北校区-观湖苑5-609-4",
+        "photo" : null,
+        "dept" : "机电工程学院"
+      },
+      "isPress" : null,
+      "isReport" : "0",
+      "recordStatus" : "8",
+      "notOutReport" : {
+        "setedNotOutReport" : 1,
+        "rule" : 0,
+        "operatorWid" : "92181062",
+        "operatorId" : "ampadmin",
+        "operatorName" : "ampadmin",
+        "operatorDate" : "2023-04-24 19:47:17"
+      },
+      "nowTime" : "2025-03-11 17:50:26",
+      "termination" : {
+        "terminationObjectName" : null,
+        "terminationDate" : null,
+        "allowTerminate" : 0,
+        "terminationReason" : null,
+        "terminated" : 0
+      },
+      "isAllowedExtend" : false,
+      "leaveExtends" : [
+  
+      ],
+      "reportPlaces" : [
+        {
+          "radius" : "540",
+          "counName" : "ampadmin",
+          "latitude" : "30.466056090983283",
+          "longitude" : "114.09378504824384",
+          "wid" : "1518",
+          "address" : "武汉商学院"
+        },
+        {
+          "radius" : "380",
+          "counName" : "ampadmin",
+          "latitude" : "30.462989886326856",
+          "longitude" : "114.09355148881117",
+          "wid" : "2263",
+          "address" : "武汉商学院"
+        },
+        {
+          "radius" : "400",
+          "counName" : "ampadmin",
+          "latitude" : "30.46270972638175",
+          "longitude" : "114.09574335425552",
+          "wid" : "2264",
+          "address" : "武汉商学院"
+        }
+      ]
+    },
+    "code" : "0"
+  };
+let obj = template;
 let datas = obj.datas;
 let detail = datas.detail;
 datas.recordStatus = "6";
@@ -23,5 +343,5 @@ detail.leaveType = datas.leaveType.name;
 detail.nowTime = "2025-03-11 18:01:03";
 detail.approvers[0].createTime = detail.logInfos[0].createTime;
 detail.approvers[1].createTime = detail.logInfos[1].createTime;
-
+console.log(obj);
 $done({body:JSON.stringify(obj)})
