0f81a7d6603436ae9ac963a84f7cf29509ac2640
Simprole <29875719+cui0v0@users.noreply.github.com>
2025-03-13 13:14:03 +0800

    Update qingjia.js

--- qingjia.js
+++ qingjia.js
@@ -6,13 +6,13 @@ datas.studentInfo.userName = "李文卓";
 detail.approvers[0].approver.userName = datas.studentInfo.userName;
 detail.approvers[1].approver.userName = "余欢";
 detail.leaveReason = "陪同王博恒去医院看病";
-detail.logInfos[0].createTime = "03-11 12:12";
-detail.logInfos[1].createTime = "03-11 19:13";
-detail.startTime = "03-11 12:12";
+detail.logInfos[0].createTime = "03-13 14:00";
+detail.logInfos[1].createTime = "03-13 20:00";
+detail.startTime = detail.logInfos[0].createTime;
 detail.startDate = detail.startTime;
-detail.endTime = "03-11 19:13";
+detail.endTime = detail.logInfos[1].createTime;
 detail.actEndTime = detail.endTime;
-detail.actEndTimeDesc = "7小时1分钟";
+detail.actEndTimeDesc = "6小时";
 detail.totalDays = detail.actEndTimeDesc;
 if(detail.applyAttach.length>0){
     detail.applyAttach = [];
@@ -20,7 +20,7 @@ if(detail.applyAttach.length>0){
 detail.leaveName = "病假";
 detail.dormitoryInfo = "北校区-观湖苑5-613-1",
 detail.leaveType = datas.leaveType.name;
-detail.nowTime = "2025-03-11 18:01:03";
+detail.nowTime = "2025-03-11 13:13:03";
 detail.approvers[0].createTime = detail.logInfos[0].createTime;
 detail.approvers[1].createTime = detail.logInfos[1].createTime;
 
